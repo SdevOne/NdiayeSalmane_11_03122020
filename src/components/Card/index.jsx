8e8c9ecc57bc0fa6f3f89954326f2efbcf9c21a0
Sd <sdevone@outlook.fr>
2021-09-19 04:40:12 +0200

    addition of the card component

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import "../../styles/Card.css"
+
+export default class Card extends Component {
+  state = { data: {} }
+
+  componentDidMount() {
+    const fetchData = async () => {
+      const response = await fetch(`/api/data.json`)
+      const jsonRes = await response.json()
+      if (jsonRes) {
+        this.setState({ data: jsonRes })
+      }
+    }
+    fetchData()
+  }
+
+  render() {
+    const { data } = this.state
+
+    return (
+      <section className="housing-container">
+        {data &&
+          Object.values(data).map(function (elt, idx) {
+            return (
+              <Link to={`/Logement/${elt.id}`} key={elt.id}>
+                <div className="card">
+                  <div className="card-filter"></div>
+                  <img className="card-img" src={elt.cover} alt="" />
+                  <h2 className="card-title">{elt.title}</h2>
+                </div>
+              </Link>
+            )
+          })}
+      </section>
+    )
+  }
+}

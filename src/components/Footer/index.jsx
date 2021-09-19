161b2e9f7a437a70f3c7cc4ffb7637b716637e40
Sd <sdevone@outlook.fr>
2021-09-19 04:42:25 +0200

    addition of the footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import logo2 from "../../assets/logo2.png"
+import "../../styles/Footer.css"
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <img className="footer-logo" src={logo2} alt="" />
+        <p className="footer-title">© 2020 Kasa. All rights reserved</p>
+      </footer>
+    )
+  }
+}
